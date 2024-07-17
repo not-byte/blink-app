590e32990f414691e5ca17cd9dacb4cd5bfe4e7c
botprzemek <info@botprzemek.pl>
2024-07-17 16:14:57 +0200

    feat(app): added colors to tailwind config

---
+++ tailwind.config.ts
@@ -0,0 +1,15 @@
+import type { Config } from 'tailwindcss'
+
+export default {
+  content: [],
+  theme: {
+    colors: {
+      smoke: "hsla(0, 0%, 96%, 100%)",
+      lavender: "hsla(240, 33%, 86%, 100%)",
+      "slate-blue": "hsla(263, 100%, 68%, 100%)",
+      "ultra-violet": "hsla(254, 31%, 36%, 100%)",
+      "rasin-black": "hsla(240, 7%, 13%, 100%)",
+    },
+  },
+  plugins: [],
+} satisfies Config
