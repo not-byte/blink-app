796a378e05a163fdf3dd18e267cc175599272716
Braspi <shutmer@gmail.com>
2024-07-21 01:15:01 +0200

    feat(app): rm files and add colors to tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -8,6 +8,13 @@ export default {
     extend: {
       fontFamily: {
         display: "Inter"
+      },
+      colors: {
+        smoke: "hsla(0, 0%, 96%, 1)",
+        lavender: "hsla(240, 33%, 86%, 1)",
+        "slate-blue": "hsla(263, 100%, 68%, 1)",
+        "ultra-violet": "hsla(254, 31%, 36%, 1)",
+        "rasin-black": "hsla(240, 7%, 13%, 1)",
       }
     },
   },
